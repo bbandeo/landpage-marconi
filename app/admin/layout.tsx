fa49913d710de2190ffaba322fe149289e6f9457
Bruno Bandeo <59704880+bbandeo@users.noreply.github.com>
2025-07-05 05:16:47 -0300

    update app/admin/layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
-import AdminLayout from "@/components/admin/AdminLayout";
+import AdminLayout from '@/components/admin/AdminLayout'
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return <AdminLayout>{children}</AdminLayout>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <AdminLayout>{children}</AdminLayout>
 }
